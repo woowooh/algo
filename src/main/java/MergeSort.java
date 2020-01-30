b6b1a69a9a4fcdd5e9d511466ddb1cfeb2b97f24
eva <>
2020-01-30 16:52:30 +0800

    添加归并，快排

---
+++ src/main/java/MergeSort.java
@@ -0,0 +1,60 @@
+
+
+public class MergeSort {
+    public static int[] brr;
+    public static void mergeSort(int[] arr) {
+        brr = new int[arr.length];
+        mergeSortInternal(arr, 0, arr.length - 1);
+    }
+
+
+    public static void mergeSortInternal(int[] arr, int s, int e) {
+        if (s >= e) {
+            return;
+        }
+        int mid = s + ((e - s) / 2);
+        mergeSortInternal(arr, s, mid);
+        mergeSortInternal(arr, mid + 1, e);
+        merge(arr, s, mid, e);
+    }
+
+    public static void merge(int[] arr, int s, int mid, int e) {
+        int i = s;
+        int j = mid + 1;
+        int k = s;
+        while (i <= mid && j <= e) {
+            if (arr[i] > arr[j]) {
+                brr[k] = arr[j];
+                j++;
+            } else {
+                brr[k] = arr[i];
+                i++;
+            }
+            k++;
+        }
+        if (i > mid) {
+            while (j <= e) {
+                brr[k] = arr[j];
+                j++;
+                k++;
+            }
+        } else {
+            while (i <= mid) {
+                brr[k] = arr[i];
+                i++;
+                k++;
+            }
+        }
+        for (i = s; i <= e; i++) {
+            arr[i] = brr[i];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 3, 1, 4, 9, 6, 5, 2};
+        mergeSort(arr);
+        for (int i: arr) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/main/java/QuickSort.java
@@ -0,0 +1,41 @@
+
+
+public class QuickSort {
+    public static void quickSort(int[] arr) {
+        quickSortInternal(arr, 0, arr.length - 1);
+    }
+
+    public static void quickSortInternal(int[] arr, int s, int e) {
+        if (s >= e) {
+            return;
+        }
+        int p = povit(arr, s, e);
+        quickSortInternal(arr, 0, p - 1);
+        quickSortInternal(arr, p + 1, e);
+    }
+
+    public static int povit(int[] arr, int s, int e) {
+        int p = arr[e];
+        int k = s;
+        for (int i = s; i < e; i++) {
+            if (p > arr[i]) {
+                int tmp = arr[i];
+                arr[i] = arr[k];
+                arr[k] = tmp;
+                k++;
+            }
+        }
+        int tmp = arr[k];
+        arr[k] = p;
+        arr[e] = tmp;
+        return k;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 3, 1, 4, 9, 6, 5, 2};
+        quickSort(arr);
+        for (int i: arr) {
+            System.out.println(i);
+        }
+    }
+}
