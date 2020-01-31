330799636f2f59605ed5a16614881ff6474b874f
eva <>
2020-01-31 15:08:48 +0800

    binarySearch loop

--- src/main/java/BinarySearch.java
+++ src/main/java/BinarySearch.java
@@ -17,9 +17,25 @@ public class BinarySearch {
         }
     }
 
+    public static int binarySearchLoop(int[] arr, int s, int e, int n) {
+        int mid;
+        while (s <= e) {
+            mid = s + (e - s) / 2;
+            if (arr[mid] > n) {
+               e = mid - 1;
+            } else if (arr[mid] < n) {
+               s = mid + 1;
+            } else {
+               return mid;
+            }
+        }
+        mid = -1;
+        return mid;
+    }
+
     public static void main(String[] args) {
-        int[] arr = {1, 2};
-        int r = binarySearch(arr, 3);
+        int[] arr = {1};
+        int r = binarySearchLoop(arr, 0, arr.length - 1, 2);
         System.out.println(r);
     }
 }
