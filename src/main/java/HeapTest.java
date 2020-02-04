1ed3dbc90c4c295dc0e0b608276b032b523bf6a6
eva <>
2020-02-04 15:41:49 +0800

    示例

--- src/main/java/HeapTest.java
+++ src/main/java/HeapTest.java
@@ -1,15 +1,21 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class HeapTest {
     public static void main(String[] args) {
-        Integer[] target = { 2, 3, 5, 8, 11, 22, 97, 98, 33, 44, 55, 66, 77, 88, 99, 100, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
-        Queue<Integer> queue = new PriorityQueue<Integer>(5);
+        Integer[] target = { 98, 0, 2, 3, 5, 8, 11, 22, 97, 98, 33, 44, 55, 66, 77, 88, 99, 100, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+        Queue<Integer> queue = new PriorityQueue<Integer>(5, new Comparator<Integer>() { //大顶堆，容量11
+            @Override
+            public int compare(Integer i1,Integer i2){
+                return i1 - i2;
+            }
+        });
         queue.addAll(Arrays.asList(1, 2, 3, 4, 5));
         for (Integer num : target) {
             Integer top = queue.peek();
-            if (num >= top) {
+            if (num > top) {
                 queue.poll();
                 queue.add(num);
             }
