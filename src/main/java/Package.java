00d6f5d025c0a2df85a75de671a0e4bcfd155864
eva <>
2020-02-05 21:36:17 +0800

    背包问题

--- src/main/java/Package.java
+++ src/main/java/Package.java
@@ -3,16 +3,22 @@
 public class Package {
 
 
-    public int maxW = Integer.MIN_VALUE;
-    // f(0, 0, a, 10, 100)；i 已扫描的物品，cw 已装物品总重量，items 物品列表，背包可用容量
-    public void f(int i, int cw, int[] items, int n, int w) {
+    public static int maxW = Integer.MIN_VALUE;
+
+    // i 已扫描的物品，cw 已装物品总重量，items 物品列表，背包可用容量
+    public static void f(int i, int cw, int[] items, int n, int w) {
         if (cw == w || i == n) {
             if (cw > maxW) maxW = cw;
             return; // 已满，或物品都装完
         }
-        f(i+1, cw, items, n, w);
+        f(i + 1, cw, items, n, w);
         if (cw + items[i] <= w) {
-            f(i+1,cw + items[i], items, n, w);
+            f(i + 1, cw + items[i], items, n, w);
         }
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[10];
+        f(0, 0, a, 10, 100);
     }
 }
