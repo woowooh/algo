2d76e94226114bd0cc2789abf0623340c351469c
eva <>
2020-02-05 15:22:47 +0800

    dfs

---
+++ src/main/java/Graph.java
@@ -0,0 +1,102 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+public class Graph {
+    private int v;
+    private LinkedList<Integer> adj[];
+
+    public Graph(int v) {
+        this.v = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; i++) {
+            adj[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int s, int t) {
+        adj[s].add(t);
+        adj[t].add(s);
+    }
+
+    public void bfs(int s, int t) {
+        if (s == t) {
+            return;
+        }
+        boolean[] visited = new boolean[v];
+        visited[s] = true;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(s);
+        int[] prev = new int[v];
+        for (int i = 0; i < v; i++) {
+            prev[i] = -1;
+        }
+        while (queue.size() != 0) {
+            int w = queue.poll();
+            for (int i = 0; i < adj[w].size(); i++) {
+                int q = adj[w].get(i);
+                if (!visited[q]) {
+                    prev[q] = w;
+                    if (q == t) {
+                        print(prev, s, t);
+                        return;
+                    }
+                }
+                visited[q] = true;
+                queue.add(q);
+            }
+        }
+    }
+
+    public void print(int[] prev, int s, int t) {
+        if (prev[t] != -1 && s != t) {
+            print(prev, s, prev[t]);
+        }
+        System.out.println(t + " ");
+    }
+
+
+    static boolean found = false;
+
+    public void dfs(int s, int t) {
+        found = false;
+        boolean[] visited = new boolean[v];
+        int[] prev = new int[v];
+        for (int i = 0; i < v; ++i) {
+            prev[i] = -1;
+        }
+        recurDfs(s, t, visited, prev);
+        print(prev, s, t);
+    }
+
+    private void recurDfs(int w, int t, boolean[] visited, int[] prev) {
+        if (found == true) return;
+        visited[w] = true;
+        if (w == t) {
+            found = true;
+            return;
+        }
+        for (int i = 0; i < adj[w].size(); ++i) {
+            int q = adj[w].get(i);
+            if (!visited[q]) {
+                prev[q] = w;
+                recurDfs(q, t, visited, prev);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Graph g = new Graph(10);
+        g.addEdge(0, 1);
+        g.addEdge(0, 3);
+        g.addEdge(1, 2);
+        g.addEdge(1, 4);
+        g.addEdge(2, 5);
+        g.addEdge(3, 4);
+        g.addEdge(4, 5);
+        g.addEdge(4, 6);
+        g.addEdge(5, 7);
+        g.addEdge(6, 7);
+        g.bfs(0, 4);
+    }
+}
